cf9e34d203f9356fd24e005ca717a427ae3e3d1a
Giulio Fagioli <fagioli.giulio@gmail.com>
2025-10-29 18:54:37 +0100

    fix: descr generation

--- scripts/generate-metadata.js
+++ scripts/generate-metadata.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const IMAGE_EXT = [".jpg", ".jpeg", ".png", ".webp", ".gif"];
-const EXIF_SCHEMA_VERSION = 3;
+const EXIF_SCHEMA_VERSION = 4;
 
 // ---------- helper ----------------------------------------------------------
 
